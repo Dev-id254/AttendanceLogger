788dc12ebc8cb59adb6f39041893b75cd5437c00
Jr <nyambaredavid3@gmail.com>
2026-02-24 14:11:24 +0300

    modified:   FrontEnd/Dashboard.html 	new file:   FrontEnd/Dashboard.js

---
+++ FrontEnd/Dashboard.js
@@ -0,0 +1,12 @@
+// set up the table..
+// Lec Mode will be the one to add or remove students in the list + other more functions
+
+document.getElementById('lec_mode').onclick = function(){
+    window.alert("Lec Mode is not set up yet, click ok to continue.");
+}
+
+// Fix Here.
+document.getElementsByClassName('info-btn').onclick = function(){
+   // something
+   window.alert('Later');
+}
